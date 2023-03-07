8e284ae218170b4d2a7a6cd99a8ec1f6272ad7a5
Chipego Kalinda <kalinda.chipego.12@gmail.com>
2023-03-07 23:38:46 +0700

    Fixed

--- MyMovieAPI/Models/Actors.cs
+++ MyMovieAPI/Models/Actors.cs
@@ -3,9 +3,9 @@
     public class Actors
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public []string Movies { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string[] Movies { get; set; } = new string[0];
         
-        public Actors() { }
+       // public Actors() { }
     }
 }
