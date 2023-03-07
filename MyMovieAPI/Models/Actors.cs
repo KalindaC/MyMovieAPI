075d929260675bec929abafe820d9354000d88da
Chipego Kalinda <kalinda.chipego.12@gmail.com>
2023-03-07 23:17:13 +0700

    Added Movie and Actor Models

---
+++ MyMovieAPI/Models/Actors.cs
@@ -0,0 +1,11 @@
+﻿namespace MyMovieAPI.Models
+{
+    public class Actors
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public []string Movies { get; set; }
+        
+        public Actors() { }
+    }
+}
---
+++ MyMovieAPI/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿namespace MyMovieAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedDate{ get; set;}
+
+        public DateTime UpdatedDate { get; set;}
+        public int ActorId { get; set; }
+
+    }
+}
